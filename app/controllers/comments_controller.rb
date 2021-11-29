c206c73e086e13d5263b79b598ad655ec92dbe52
MotoSaku <gracegloria63@gmail.com>
2021-11-29 17:44:27 +0900

    修正依頼箇所の修正
    ・コメントフォームを空で投稿した際に、そのページに留まるように修正
    ・ログイン・アウト状態に関わらず、ユーザー詳細ページでは、そのユーザーの投稿のみを表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,7 @@ def create
       redirect_to prototype_path(@comment.prototype)
     else
       @prototype = @comment.prototype
+      @comments = @prototype.comments
       render "prototypes/show"
     end
 end
